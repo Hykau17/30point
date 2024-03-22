fd592a01912382fdb02a2fdd469775b3c68ee9d7
Hykau17 <163821150+Hykau17@users.noreply.github.com>
2024-03-22 22:04:17 +0700

    Update and rename from dang ky to dangkytour.php

---
+++ dangkytour.php
@@ -0,0 +1,22 @@
+<form method="post">
+<input type="hidden" name="my_form" value="my_form">
+
+<label for="Số Điện THoại" style="color: orange">Phone:</label>
+<input type="NUMBER" name="phone" required="">
+
+<label for="NAME" style="color: orange">Name:</label>
+<input type="TEXT" name="name" required="">
+
+<label for="email" style="color: orange">Email:</label>
+<input type="EMAIL" name="email" required="">
+
+<label for="thoigiandukien" style="color: orange">Thời Gian Khởi Hành :</label>
+<input type="date" name="thoigiandukien" required="">
+
+<label for="MESSAGE" style="color: orange">Tour Muốn Đăng Ký</label>
+<textarea name="message" required=""></textarea>
+
+
+<br>
+<input type="submit" value="Submit">
+</form>
